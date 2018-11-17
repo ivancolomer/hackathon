0f54358acd46f9b8a5b06fc8ce55ee7bf7febb68
AleksSG <samuilov.aleks@gmail.com>
2018-11-17 13:54:40 +0100

    go to alerts
    changes in main that allows you to go to th enext activity

--- app/src/main/java/com/ibar/protectme/MainActivity.java
+++ app/src/main/java/com/ibar/protectme/MainActivity.java
@@ -1,13 +1,29 @@
 package com.ibar.protectme;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = MainActivity.this;
+
+        goToAlertsButtonListener();
+    }
+
+    private void goToAlertsButtonListener() {
+        Button alertsButton = findViewById(R.id.buttonToAlerts);
+        alertsButton.setOnClickListener(view -> {
+            Intent intent = new Intent(context, AlertsSectionActivity.class);
+            startActivity(intent);
+        });
     }
 }
